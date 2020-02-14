68f84521304a330e2e033a28bdfaa7f916b495b1
yuta519 <yuta519.ka23tf@docomo.ne.jp>
2020-02-14 16:00:49 +0900

    modified

--- lookupUrlClassification.py
+++ lookupUrlClassification.py
@@ -10,9 +10,12 @@ import re
 
 ### Main Functions ###
 def main():
-    # Create API Cookie
-    value      = getApiSession.obfuscateApiKey()
-    api_cookie = getApiSession.getCookie(value)
+    # Account Information
+    id_value = settings.getId()
+    api_key  = id_value[0]
+    username = id_value[1] 
+    password = id_value[2]
+    hostname = id_value[3]
 
     # Receive and Parse Standard Input
     target_urls = sys.argv
@@ -21,14 +24,23 @@ def main():
         print('Please Input URL')
         sys.exit()
     else:
+        # Parse Input URL 
         parsed_target_urls = []
         for i in range(0, len(target_urls)):
             domain = checkUrl(target_urls[i])
             parsed_url = urlparse(target_urls[i])
             parsed_target_urls.append(domain)
-        lookupUrlClassification(api_cookie, parsed_target_urls)
+        
+        # Create API Cookie
+        obfuscate_api_key = getApiSession.obfuscateApiKey(api_key)
+        api_cookie = getApiSession.getCookie(hostname, obfuscate_api_key, username, password)
 
-### Extract Domain from Standard Input ###
+        response = lookupUrlClassification(hostname, api_cookie, parsed_target_urls)
+        for res in response:
+            print(res)
+
+
+### Extract Domain from Standard Input ("HTTPS" and "HTTP" are excluded)###
 def checkUrl(url):
     pattern = "https?://[\w/:%#\$&\?\(\)~\.=\+\-]+"
     if re.match(pattern, url):
@@ -37,27 +49,20 @@ def checkUrl(url):
     return url
 
 ### Lookup URL Classification by Zscaler ###
-def lookupUrlClassification(cookie, urls):
-
-    ### Environment Information ###
-    id_value = settings.getId()
-    hostname = id_value[3]
-
+def lookupUrlClassification(hostname, cookie, urls):
     url = 'https://'+hostname+'/api/v1/urlLookup'
     headers = {
         'content-type': "application/json",
         'cache-control': "no-cache",
         'cookie': cookie
     }
-    payload = urls
-
     response=requests.post( 
         url,
-        json.dumps(payload),
+        json.dumps(urls),
         headers=headers
     )
-    pprint.pprint(response.json())
-
+    res = response.json()
+    return res
 
 ### Exection ###
 if __name__ == '__main__':
