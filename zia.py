311f8f95bbb0c0ac970cc94d4c885ea1c7f98a48
yuta519 <yuta519.ka23tf@docomo.ne.jp>
2021-08-30 11:26:52 +0900

    refactor zia.py from black

--- zia.py
+++ zia.py
@@ -8,18 +8,23 @@ import requests
 
 
 class Zia(object):
-
-    def __init__(self, cloud_name, api_key, admin_username, admin_password,
-                api_token=''):
+    def __init__(
+        self,
+        cloud_name: str,
+        api_key: str,
+        admin_username: str,
+        admin_password: str,
+        api_token: str = "",
+    ) -> None:
         self.cloud_name = cloud_name
         self.api_key = api_key
         self.admin_username = admin_username
         self.admin_password = admin_password
-        self.base_url = 'https://admin.{}/api/v1'.format(cloud_name)
+        self.base_url = f"https://admin.{cloud_name}/api/v1"
         self.api_token = api_token
 
     def obfuscateApiKey(self):
-        """Parse API Key to use zscaler api. 
+        """Parse API Key to use zscaler api.
         This functions are supplied by Zscaler.
         More information of this function is below.
         Reference : Zscaler help pages.
@@ -33,30 +38,24 @@ class Zia(object):
         for i in range(0, len(str(n)), 1):
             key += seed[int(str(n)[i])]
         for j in range(0, len(str(r)), 1):
-            key += seed[int(str(r)[j])+2]
+            key += seed[int(str(r)[j]) + 2]
         obfuscate_api_key = [now, key]
         return obfuscate_api_key
-    
+
     def login(self):
         """Login to Zscaler and create an api session."""
         obfuscate_api_key = self.obfuscateApiKey()
-        api_endpoint = '{}/authenticatedSession'.format(self.base_url)
-        headers = {
-            'content-type': "application/json",
-            'cache-control': "no-cache"
-        }
+        api_endpoint = "{}/authenticatedSession".format(self.base_url)
+        headers = {"content-type": "application/json", "cache-control": "no-cache"}
         payload = {
-            'username': self.admin_username,
-            'password': self.admin_password,
-            'timestamp': obfuscate_api_key[0],
-            'apiKey': obfuscate_api_key[1]
+            "username": self.admin_username,
+            "password": self.admin_password,
+            "timestamp": obfuscate_api_key[0],
+            "apiKey": obfuscate_api_key[1],
         }
-        response = requests.post(
-            api_endpoint,
-            json.dumps(payload),
-            headers=headers)
+        response = requests.post(api_endpoint, json.dumps(payload), headers=headers)
         cookie = response.cookies.get_dict()
-        api_token = 'JSESSIONID=' + cookie['JSESSIONID']
+        api_token = "JSESSIONID=" + cookie["JSESSIONID"]
         self.api_token = api_token
 
     def activate_configuration(self):
@@ -66,21 +65,21 @@ class Zia(object):
 
     def logout(self):
         """Logout from API sesion."""
-        api_endpoint = '{}/authenticatedSession'.format(self.base_url)
+        api_endpoint = "{}/authenticatedSession".format(self.base_url)
         headers = {
-            'content-type': "application/json",
-            'cache-control': "no-cache", 
-            'cookie': self.api_token
+            "content-type": "application/json",
+            "cache-control": "no-cache",
+            "cookie": self.api_token,
         }
         requests.delete(api_endpoint, headers=headers)
 
 
 class ZiaLookupUrl(Zia):
     """Handle the api to lookup a given url."""
-    
+
     def extract_url_domain(self, target_url):
         """Extract domain from url given by user."""
-        url_pattern = 'https?://[\w/:%#\$&\?\(\)~\.=\+\-]+'
+        url_pattern = "https?://[\w/:%#\$&\?\(\)~\.=\+\-]+"
         if match(url_pattern, target_url):
             parsed_url = urlparse(target_url)
             domain = parsed_url.netloc
@@ -88,21 +87,20 @@ class ZiaLookupUrl(Zia):
         else:
             return target_url
 
-    def get_url_categories(self, mode='all'):
+    def get_url_categories(self, mode="all"):
         """Get Zscaler's url catergories."""
         self.login()
-        if mode == 'all':
-            api_endpoint = '{}/urlCategories'.format(self.base_url)
-        elif mode == 'customonly':
-            api_endpoint = '{}/urlCategories?customOnly=true'.format(
-                            self.base_url)
+        if mode == "all":
+            api_endpoint = "{}/urlCategories".format(self.base_url)
+        elif mode == "customonly":
+            api_endpoint = "{}/urlCategories?customOnly=true".format(self.base_url)
         else:
-            print('wrong option name : {}'.format(mode))
+            print("wrong option name : {}".format(mode))
             exit()
         headers = {
-            'content-type': 'application/json',
-            'cache-control': 'no-cache', 
-            'cookie': self.api_token
+            "content-type": "application/json",
+            "cache-control": "no-cache",
+            "cookie": self.api_token,
         }
         response = requests.get(api_endpoint, headers=headers)
         self.logout()
@@ -114,17 +112,16 @@ class ZiaLookupUrl(Zia):
         parsed_target_urls = []
         for target_url in target_urls:
             parsed_target_url = self.extract_url_domain(target_url)
-            parsed_target_urls.append(parsed_target_url)        
-        api_endpoint = '{}/urlLookup'.format(self.base_url)
+            parsed_target_urls.append(parsed_target_url)
+        api_endpoint = "{}/urlLookup".format(self.base_url)
         headers = {
-            'content-type': "application/json",
-            'cache-control': "no-cache",
-            'cookie': self.api_token
+            "content-type": "application/json",
+            "cache-control": "no-cache",
+            "cookie": self.api_token,
         }
-        response = requests.post( 
-            api_endpoint,
-            json.dumps(parsed_target_urls),
-            headers=headers)
+        response = requests.post(
+            api_endpoint, json.dumps(parsed_target_urls), headers=headers
+        )
         json_results = response.json()
         self.logout()
         return json_results
