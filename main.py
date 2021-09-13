34e6fb5044b186beb26eb9b73a3b319893d7f51d
yuta519 <yuta519.ka23tf@docomo.ne.jp>
2021-09-13 12:03:59 +0900

    create function url filtering rules

--- main.py
+++ main.py
@@ -1,22 +1,35 @@
-import argparse
-import os
-
-import zia
 from admin import fetch_adminusers
 from admin import create_adminuser
-from base import Base
-
+from url_filtering_rules import create_url_filering_rules
+from url_filtering_rules import fetch_all_url_filering_rules 
 
 def main():
     # print(fetch_adminusers("yoshiyuki.ishii@softbank-demo-05.com"))
-    create_adminuser(
-        loginName="test1@zscaler.net",
-        userName="Yuta Kawamura",
-        email="test1@zscaler.net",
-        password="P@ssw0rd",
-        rolename="Admin",
-    )
+    # create_adminuser(
+    #     loginName="test1@zscaler.net",
+    #     userName="Yuta Kawamura",
+    #     email="test1@zscaler.net",
+    #     password="P@ssw0rd",
+    #     rolename="Admin",
+    # )
 
+    # print(fetch_all_url_filering_rules())
+
+
+
+    print(create_url_filering_rules(
+        name="Test Filtering Rule",
+        order=1,
+        protocols=["HTTPS_RULE"],
+        locations=[],
+        groups=[],
+        departments=[],
+        users=[],
+        url_categories=["GAMBLING"],
+        state="DISABLED",
+        rank=0,
+        action="ALLOW",
+    ))
 
 if __name__ == "__main__":
     main()
