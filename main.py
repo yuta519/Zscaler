69231b2b8b155e0b19306c95434b9c9997b21af7
yuta519 <yuta519.ka23tf@docomo.ne.jp>
2021-09-15 14:28:45 +0900

    add fetching url categories func

--- main.py
+++ main.py
@@ -2,9 +2,9 @@ from admin import fetch_adminusers
 from admin import create_adminuser
 from url_filtering_rules import create_url_filering_rules
 from url_filtering_rules import fetch_all_url_filering_rules 
+from url_categories import get_url_categories 
 
 def main():
-    # print(fetch_adminusers("yoshiyuki.ishii@softbank-demo-05.com"))
     # create_adminuser(
     #     loginName="test1@zscaler.net",
     #     userName="Yuta Kawamura",
@@ -15,21 +15,20 @@ def main():
 
     # print(fetch_all_url_filering_rules())
 
-
-
-    print(create_url_filering_rules(
-        name="Test Filtering Rule",
-        order=1,
-        protocols=["HTTPS_RULE"],
-        locations=[],
-        groups=[],
-        departments=[],
-        users=[],
-        url_categories=["GAMBLING"],
-        state="DISABLED",
-        rank=0,
-        action="ALLOW",
-    ))
+    # create_url_filering_rules(
+    #     name="Test Filtering Rule",
+    #     order=1,
+    #     protocols=["HTTPS_RULE"],
+    #     locations=[],
+    #     groups=[],
+    #     departments=[],
+    #     users=[],
+    #     url_categories=["GAMBLING"],
+    #     state="DISABLED",
+    #     rank=0,
+    #     action="ALLOW",
+    # )
+    get_url_categories()
 
 if __name__ == "__main__":
     main()
